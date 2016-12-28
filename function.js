89623b70dec7782e41ae833890be0a1c24e78114
Tuanhao <tuanhao256@gmail.com>
2016-12-28 00:22:34 -0500

    add project page

--- function.js
+++ function.js
@@ -2,6 +2,14 @@ var main = function() {
   $('.dropdown-toggle').click(function() {
     $('.dropdown-menu').toggle();
   });
+  $('.button').click(function() {
+    $('.button').animate({
+        left: '250px',
+        opacity: '0.5',
+        height: '150px',
+        width: '150px'
+    });
+  });
 }
 
 $(document).ready(main);
